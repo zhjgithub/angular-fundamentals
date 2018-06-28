a49d2876dbc7e1fa843fc250d3590a56c8c041fa
organism <neuqzhj@live.com>
2018-06-28 17:50:06 +0800

    feature: use service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { GitSearchService } from './git-search.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'GitHub Browser';
+
+  constructor(private GitSearchService: GitSearchService) {}
+
+  ngOnInit() {
+    this.GitSearchService.gitSearch('angular').then(
+      response => alert('Total Libraries Found: ' + response.total_count),
+      error => alert('Error: ' + error.statusText)
+    );
+  }
 }
