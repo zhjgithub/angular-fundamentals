797cdf7c7b02706005de361b67021b25bcdab492
organism <neuqzhj@live.com>
2018-06-29 15:27:06 +0800

    feature: add component to display git search results

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,14 +11,5 @@ export class AppComponent implements OnInit {
 
   constructor(private GitSearchService: GitSearchService) {}
 
-  ngOnInit() {
-    this.GitSearchService.gitSearch('angular').then(
-      response => alert('Total Libraries Found: ' + response.total_count),
-      error => alert('Error: ' + error.statusText)
-    );
-    this.GitSearchService.gitUser('angular').then(
-      response => alert('Total Users Found: ' + response.total_count),
-      error => alert('Error: ' + error.statusText)
-    );
-  }
+  ngOnInit() {}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from "@angular/common/http";
 import { AppComponent } from './app.component';
+import { GitSearchComponent } from './git-search/git-search.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GitSearchComponent
   ],
   imports: [
     BrowserModule,
