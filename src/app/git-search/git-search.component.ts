f20e70d2b1967cd21403e6725295d49ddceb194f
organism <neuqzhj@live.com>
2018-06-29 21:44:00 +0800

    feature: update route pages

--- src/app/git-search/git-search.component.ts
+++ src/app/git-search/git-search.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { GitSearch } from '../git-search';
 import { GitSearchService } from '../git-search.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-git-search',
@@ -10,7 +11,11 @@ import { GitSearchService } from '../git-search.service';
 export class GitSearchComponent implements OnInit {
   searchResults: GitSearch;
   searchQuery: string;
-  constructor(private GitSearchService: GitSearchService) {}
+  title: string;
+  constructor(
+    private GitSearchService: GitSearchService,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit() {
     this.GitSearchService.gitSearch('angular').then(
@@ -21,6 +26,7 @@ export class GitSearchComponent implements OnInit {
         alert('Error: ' + error.statusText);
       }
     );
+    this.route.data.subscribe(result => (this.title = result.title));
   }
 
   gitSearch() {
